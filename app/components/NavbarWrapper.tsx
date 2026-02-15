d92ac71c7922a5a7900249fe8e72ce4c6aaa7c92
Aman Jaiswal <jais.aman03@gmail.com>
2026-02-15 11:47:39 +0100

    Refactor NavbarWrapper component to remove props and simplify rendering of Navbar within Suspense for improved clarity.

--- app/components/NavbarWrapper.tsx
+++ app/components/NavbarWrapper.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import Navbar from "./Navbar";
 
-export default function NavbarWrapper(props: React.ComponentProps<typeof Navbar>) {
+export default function NavbarWrapper() {
   return (
     <Suspense fallback={<div className="h-20" />}>
-      <Navbar {...props} />
+      <Navbar />
     </Suspense>
   );
 }
