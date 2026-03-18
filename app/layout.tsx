6a8b2c7b317b266871a04301bba40451e3930aaa
Aman Jaiswal <jais.aman03@gmail.com>
2026-03-18 17:53:42 +0100

    Update Google Tag Manager script ID in layout component: change tracking ID to enhance analytics capabilities.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,41 +3,41 @@ import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title:
-    "Advanced AI-powered legal case outcome analysis and scenario prediction model",
-  description:
-    "We aim to simplify litigation so that anyone, regardless of legal background, can be understood on the merits of their case. By reducing cost barriers and procedural complexity, we empower individuals and professionals to pursue fair outcomes through TheLawThing.",
-  icons: {
-    icon: [{ url: "/favicon.ico", sizes: "any" }],
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
-  },
+	title:
+		"Advanced AI-powered legal case outcome analysis and scenario prediction model",
+	description:
+		"We aim to simplify litigation so that anyone, regardless of legal background, can be understood on the merits of their case. By reducing cost barriers and procedural complexity, we empower individuals and professionals to pursue fair outcomes through TheLawThing.",
+	icons: {
+		icon: [{ url: "/favicon.ico", sizes: "any" }],
+		shortcut: "/favicon.ico",
+		apple: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=AW-18007731705`}
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=AW-17430668981`}
+					strategy="afterInteractive"
+				/>
+				<Script id="gtag-init" strategy="afterInteractive">
+					{`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-18007731705');
+            gtag('config', 'AW-17430668981');
           `}
-        </Script>
-      </head>
-      <body className="antialiased" suppressHydrationWarning>
-        {children}
-      </body>
-    </html>
-  );
+				</Script>
+			</head>
+			<body className="antialiased" suppressHydrationWarning>
+				{children}
+			</body>
+		</html>
+	);
 }
